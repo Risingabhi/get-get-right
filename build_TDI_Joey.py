50a09d4cfb46e2da4466d05804e85af0cb70a246
TDI <Risingabhi@gmail.com>
2023-11-09 18:32:33 +0530

    Update :streamlitjson

--- build_TDI_Joey.py
+++ build_TDI_Joey.py
@@ -5,7 +5,7 @@ with special reference to mathmatical logic behind code.
 2- Analyse the users metric, analyse how many times he asks ai and then gives a good rating,'''
 #temp weblink wwww.getrightgit.com
 
-"""use cases target - any coder , primary focus on PYTHON codes"""
+#use cases target - any coder , primary focus on PYTHON codes
 
 import streamlit as st 
 import os
@@ -17,24 +17,71 @@ import openai
 # from openai.embeddings_utils import cosine_similarity
 import os
 import pandas
+from streamlit_lottie import st_lottie 
+import subprocess
+import queue
+from analyze_code import get_repo
+repo_queue = queue.Queue()
 
 
 
-#step1 web app that has buttons 'my-github', "analyse code","my-problem-statement", "Ai Answer"
 
 
-# Create a Streamlit app
-st.title("Get the right Git-Repo! problem?")
+# Create a Streamlit app with a red title
+st.markdown("<h1 style='color: green;'>Get the right Git-Repo! problem?</h1>", unsafe_allow_html=True)
+
+with open('coder.json','r') as f:
+    image = json.load(f)
+# Display the Lottie animation
+st_lottie(image,height=200, 
+        width=200)
 
 
-    
-# Add the button and text entry box to the app
-b1 =st.button("my-problem")
-l1=st.text_input("Tell us your problem", value="Describe clearly your problem")
-b2=st.button("my-github")
+
+#user inputs  
+problem_statement_user =[] 
+
+
+l1 = st.text_input("Tell us your problem", value="Describe clearly your problem", key="problem_input")
+if l1:
+    problem_statement_user.append(l1)
+else:
+    pass
+
 l2=st.text_input("what github repo you want to use for solving above issue", value="provide link")
-b3=st.button("analyze code")
-l3=st.text_input("Solution to your problem", value="Ai suggestion")
-b4=st.button("Ai answer")
-#st.text_input("Ai Answers", value="Ai's Answer")
 
+
+
+sp_file_input=st.text_input("specific file which you want to analyze", value="provide link")
+
+#user action.
+b3=st.button("analyze code")
+if b3:
+    print("github link", l2)
+    if l2 :
+        # Run the OpenPose command asynchronously
+        command = (
+            f"git clone {l2}"
+            
+        )
+
+        # Start the command without waiting for completion
+        repo_queue.put(l2)
+        process = subprocess.Popen(command, shell=True)
+        with open('downloading.json','r') as f:
+            image = json.load(f)
+        # Display the Lottie animation
+        st_lottie(image,height=200, 
+            width=200)
+        process.wait()  # Wait for the process to finish
+    
+    # Check if the process completed successfully
+        if process.returncode == 0:
+            st.write("Done")  # Print "Done" if the download is successful
+            get_repo(repo_queue)
+        else:
+            st.write("Download failed")  # Print an error message if the download failed
+       
+
+    else:
+        pass
